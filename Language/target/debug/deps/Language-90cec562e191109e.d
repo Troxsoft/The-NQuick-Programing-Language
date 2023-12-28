43a946c8d1ab227e9ea3094fd286ac19c53ed152
Troxsoft <troxz6@outlook.com>
2023-12-27 20:46:36 -0500

    remove target directories

--- Language/target/debug/deps/Language-90cec562e191109e.d
+++
@@ -1,9 +0,0 @@
-C:\Users\troxz\OneDrive\Documentos\dev\nQuick\Language\target\debug\deps\libLanguage-90cec562e191109e.rmeta: src\lib.rs src\GenerateCode.rs src\Lexer.rs src\LexerError.rs src\Token.rs
-
-C:\Users\troxz\OneDrive\Documentos\dev\nQuick\Language\target\debug\deps\Language-90cec562e191109e.d: src\lib.rs src\GenerateCode.rs src\Lexer.rs src\LexerError.rs src\Token.rs
-
-src\lib.rs:
-src\GenerateCode.rs:
-src\Lexer.rs:
-src\LexerError.rs:
-src\Token.rs:
